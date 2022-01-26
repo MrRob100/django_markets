f96bd4c0c6a80c4fc50d0baa47c0bc2e542f2b1d
horidhenry <robarnold100@hotmail.com>
2022-01-26 11:11:09 +0000

    save

--- django_markets/settings.py
+++ django_markets/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-@%s6r^q7^-fzx^5_g&yw_zp3739nqd3g(96atyg-5nxcyk=e&)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 
--- market_data/views.py
+++ market_data/views.py
@@ -1,7 +1,34 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import requests
+import json
+import os
 
 # Create your views here.
 
 def stock_candles(request, symbol):
-    return HttpResponse("You're looking at symbol %s." % symbol)
+
+    headers = {
+        'APCA-API-KEY-ID': os.environ.get('ALPACA_KEY'),
+        'APCA-API-SECRET-KEY': os.environ.get('ALPACA_SECRET')
+    }
+
+    r = requests.get('https://data.alpaca.markets/v2/stocks/' + symbol + '/bars', headers=headers)
+    print(os.environ.get('ALPACA_KEY'))
+
+#     return HttpResponse(json.dumps(r))
+    return HttpResponse("You're looking at symbol %s." % r.status_code)
+
+def crypto_candles(request, symbol):
+
+
+
+    return HttpResponse("You're looking at symbol " + symbol)
+#     return HttpResponse("You're looking at symbol %s." % symbol)
+
+def forex_candles(request, symbol):
+
+
+
+    return HttpResponse("You're looking at symbol " + symbol)
+#     return HttpResponse("You're looking at symbol %s." % symbol)
