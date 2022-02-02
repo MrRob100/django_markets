a9cccb9b4a263f807b7a2e18cefd94c4248db1ae
horidhenry <robarnold100@hotmail.com>
2022-02-02 17:43:04 +0000

    better data formatting

--- market_data/views.py
+++ market_data/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from datetime import datetime, timedelta
 import time
+import json
 import requests
 import os
 from .models import Candle
@@ -23,6 +24,7 @@ def stock_candles(request, symbol):
         end = (datetime.today() - timedelta(days=1)).date()
         r = requests.get('https://data.alpaca.markets/v2/stocks/' + symbol + '/bars?timeframe=1Day&start=' + str(start) + '&end=' + str(end), headers=headers)
         if r.status_code == 200:
+            Candle.objects.filter(asset_class='stock', symbol=symbol.upper()).delete()
             response = r.text
             Candle.objects.create(symbol=symbol.upper(), asset_class='stock', data=response)
         else:
@@ -32,20 +34,42 @@ def stock_candles(request, symbol):
 
 def crypto_candles(request, symbol):
     time_threshold = datetime.now() - timedelta(hours=5)
-    record = Candle.objects.filter(asset_class='crypto', symbol=symbol, date_added__gt=time_threshold)
+    record = Candle.objects.filter(asset_class='crypto', symbol=symbol.upper(), date_added__gt=time_threshold)
 
     if record.exists():
         response = record.latest('data').data
     else:
-        Candle.objects.filter(asset_class='crypto', symbol=symbol).delete()
         r = requests.get('https://www.binance.com/api/v3/klines?symbol=' + symbol.upper() + 'USDT&interval=1d')
         if r.status_code == 200:
+            Candle.objects.filter(asset_class='crypto', symbol=symbol.upper()).delete()
             response = r.text
+
             Candle.objects.create(symbol=symbol.upper(), asset_class='crypto', data=response)
         else:
             return HttpResponse(status=r.status_code)
 
-    return HttpResponse(response)
+    r2 = requests.get('https://www.binance.com/api/v3/klines?symbol=' + symbol.upper() + 'USDT&interval=1d')
+
+    list = json.loads(r2.text)
+
+    print("type \/")
+    print(type(list))
+    # fmt = [float(item) for item in list[0]]
+    
+    data = []
+    for item in list:
+        print(item)
+        # data.append([item[0]])
+        # data.append([float(item[1])])
+        # data.append([float(item[2])])
+        # data.append([float(item[3])])
+        # data.append([float(item[4])])
+
+        data.append([item[0], float(item[1]), float(item[2]), float(item[3]), float(item[4])])
+        # data.append([item[0], float(item[1]), float(item[2]), float(item[3]), float(item[4])])
+
+    # return HttpResponse(fmt)
+    return HttpResponse(json.dumps(data))
 
 def forex_candles(request, pair):
     time_threshold = datetime.now() - timedelta(hours=5)
@@ -60,6 +84,7 @@ def forex_candles(request, pair):
         formatted = pair[:3] + '-' + pair[3:]
         r = requests.get('https://finnhub.io/api/v1/forex/candle?symbol=OANDA:' + formatted.upper() + '&resolution=D&from' + str(start) + '&to=' + str(end) + '&token=' + token)
         if r.status_code == 200:
+            Candle.objects.filter(asset_class='forex', symbol=pair.upper()).delete()
             response = r.text
             Candle.objects.create(symbol=pair.upper(), asset_class='forex', data=response)
         else:
